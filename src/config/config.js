bc90dbf75bb18577d986ca51636f5e4df07a3f51
yvendee <71810479+yvendee@users.noreply.github.com>
2024-10-03 22:26:43 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
 // src/config/config.js
-export const API_URL = "http://127.0.0.1:5000";
+// export const API_URL = "http://127.0.0.1:5000/api";
+export const API_URL = "http://172.187.212.148/api";
 export const DATABASE_NAME = 'vektordata_db'; 
