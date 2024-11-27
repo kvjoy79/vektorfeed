3b3177692169a1771eecd48da84323f780b2009d
kayvendd <kayven@ddconsultingco.com>
2024-11-27 19:17:17 +0800

    feat: adding console log on the with sidebar component

--- src/components/WithSidebar/withsidebar.js
+++ src/components/WithSidebar/withsidebar.js
@@ -1,9 +1,16 @@
 // src/components/WithSidebar/withsidebar.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '../Sidebar/sidebar';
 import './withsidebar.css'; 
 
 const WithSidebar = ({ children }) => {
+  
+  // useEffect hook to log when the component is loaded
+  useEffect(() => {
+    console.log("withsidebar is loaded");
+  }, []); // The empty array ensures this runs only once when the component mounts.
+
+
   return (
     <div className="app-container">
       <Sidebar />
