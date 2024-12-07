6201a1657027436093d17bd80284e75046ac6312
kayvendd <kayven@ddconsultingco.com>
2024-12-07 09:42:34 +0800

    update: review profile table

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -27,6 +27,7 @@ const Dashboard = () => {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [dateError, setDateError] = useState('');
+  const [tableData, setTableData] = useState(null);
 
 
   const yValues = [1, 2, 3, 4, 5, 6, 10]; // Data for each day of the week (Sunday to Saturday)
@@ -246,6 +247,39 @@ const Dashboard = () => {
 
   }, [reviewId]);
 
+
+  useEffect(() => {
+    if (!reviewId) {
+      setErrorMessage('No place_id found in localStorage');
+      setIsLoading(false);
+      return;
+    }
+
+    const fetchTableData = async () => {
+      try {
+        // Make API call to fetch table data using the reviewId
+        const response = await fetch(
+          `${API_URL}/vektordata/get-review-profile-table-data?place_id=${reviewId}`
+        );
+        const data = await response.json();
+
+        if (response.ok) {
+          // Assuming the data is returned in the 'tableData' format
+          setTableData(data.tableData[0]);  // Get the first element from tableData array
+        } else {
+          setErrorMessage(data.error || 'Error fetching table data');
+        }
+      } catch (error) {
+        setErrorMessage('An error occurred while fetching data.');
+      } finally {
+        setIsLoading(false); // Set loading state to false after data is fetched
+      }
+    };
+
+    fetchTableData();
+  }, [reviewId]); // Dependency array will run this effect when reviewId changes
+
+
   // useEffect(() => {
   //   if (!reviewId) {
   //     setErrorMessage('No place_id found in localStorage');
@@ -355,20 +389,20 @@ const Dashboard = () => {
   const placenameFromLocalStorage = localStorage.getItem('place_name') || "Loading..."; // Fallback to the default value if not in localStorage
 
 
-  const tableData = [
-    {
-      placename: placenameFromLocalStorage,
-      values: [
-        [5, 3],
-        [2, 1],
-        ['', 4],
-        [3, 2],
-        [4, 6],
-      ],
-      // Hardcoded dates
-      dates: ['2024-10-19', '2024-10-20', '2024-10-21', '2024-10-22', '2024-10-23'],
-    },
-  ];
+  // const tableData = [
+  //   {
+  //     placename: placenameFromLocalStorage,
+  //     values: [
+  //       [5, 3],
+  //       [2, 1],
+  //       ['', 4],
+  //       [3, 2],
+  //       [4, 6],
+  //     ],
+  //     // Hardcoded dates
+  //     dates: ['2024-10-19', '2024-10-20', '2024-10-21', '2024-10-22', '2024-10-23'],
+  //   },
+  // ];
 
 
   // const renderSentiment = (value) => {
@@ -458,7 +492,6 @@ const Dashboard = () => {
           </div>
         </div>
       )}
-
 
 
       {/* Containers Grid */}
