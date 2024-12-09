39c1a0252dced9c91fab2f7fbfe844f8936180f9
kayvendd <kayven@ddconsultingco.com>
2024-12-10 00:07:01 +0800

    update: dashboard page

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -320,7 +320,7 @@ const Dashboard = () => {
     };
 
     fetchWeeklyRatings();
-  }, [placeId]);  // Re-run the effect if the placeId changes
+  }, []);
 
   // useEffect(() => {
   //   if (!reviewId) {
